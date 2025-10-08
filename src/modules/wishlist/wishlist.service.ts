a900f2e68363602892db30754fa020900580fadc
mmulyana <mulyan.t20@gmail.com>
2025-10-08 16:08:05 +0700

    feat: update wishlist service

--- src/modules/wishlist/wishlist.service.ts
+++ src/modules/wishlist/wishlist.service.ts
@@ -1,15 +1,22 @@
+import { differenceInDays, format } from 'date-fns'
 import { Injectable } from '@nestjs/common'
+
 import { PrismaService } from 'src/shared/prisma/prisma.service'
-import { CreateWishlistDto } from './dto/create-wishlist.dto'
 import { PaginationDto } from 'src/shared/dto/pagination.dto'
 import { paginate } from 'src/shared/utils/pagination'
+import { serialize } from 'src/shared/utils'
+
+import { CreateWishlistDto } from './dto/create-wishlist.dto'
 
 @Injectable()
 export class WishlistService {
   constructor(private db: PrismaService) {}
 
   async create(data: CreateWishlistDto) {
-    return await this.db.wishlist.create({ data })
+    const res = await this.db.wishlist.create({ data })
+    return {
+      data: serialize(res),
+    }
   }
 
   async update(id: string, data: CreateWishlistDto) {
@@ -21,7 +28,7 @@ export class WishlistService {
   }
 
   async findAll({ pagination, q }: { pagination: PaginationDto; q?: string }) {
-    return paginate({
+    const res = await paginate({
       model: this.db.wishlist,
       args: {
         where: {
@@ -40,5 +47,27 @@ export class WishlistService {
       },
       ...pagination,
     })
+
+    const formatted = res.data.map((i) => {
+      const funded = i.transaction.reduce(
+        (prev, acc) => acc.transaction.amount + prev,
+        0n,
+      )
+      const total = BigInt(i.total || 0)
+      const progress = funded === 0n ? 0 : Number((total * 100n) / funded)
+
+      return {
+        ...i,
+        deadlineAt: format(new Date(i.deadlineAt), 'dd MMM yyyy'),
+        daysLeft: differenceInDays(new Date(i.deadlineAt), new Date()),
+        progress,
+        funded,
+      }
+    })
+
+    return {
+      data: serialize(formatted),
+      meta: res.meta,
+    }
   }
 }
